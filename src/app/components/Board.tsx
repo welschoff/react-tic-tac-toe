dd10273ab38df0202ef7a492f6abfb71ef4e4992
welschoff <h.welschoff@gmx.de>
2021-11-02 16:53:56 +0100

    :sparkles: Create a Tic Tac Toe Game
    Create a Tic Tac Toe Game

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 function Board(): JSX.Element {
-  function renderSquare(i: number) {
-    return <Square />;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
   }
 
-  const status = 'Next player: X';
+  function renderSquare(i) {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+  }
+
+  // const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <div>
@@ -28,6 +48,30 @@ function Board(): JSX.Element {
       </div>
     </div>
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
 
 export default Board;
--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import { prependOnceListener } from 'process';
+import React, { useState } from 'react';
 
-function Square(): JSX.Element {
-  return <button className="square">{/* TODO */}</button>;
+function Square(props): JSX.Element {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
